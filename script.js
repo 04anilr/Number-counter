03574e32226244cf355b0b14e0d27605ea42ed36
anilr <anilr5364@gmail.com>
2024-03-10 01:02:45 +0530

    fex: add reset button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let countElement = document.getElementById("count");
 let increment = document.getElementById("increase");
 let decrement = document.getElementById("decrease");
 
+
 function addOne(){
     let currentCount = parseInt(countElement.innerText);
     countElement.innerText = currentCount + 1;
@@ -13,6 +14,10 @@ function subOne(){
          countElement.innerText = CurrentCount - 1;  
     }
 }
+document.getElementById("reset").addEventListener("click", ()=>{
+    count=0;
+    countElement.textContent = count;
+})
    
 
 increment.addEventListener("click", addOne)
